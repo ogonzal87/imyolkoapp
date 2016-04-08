997347eb93af14d826c43bbc44d696cbc7816b36
Oscar Gonzalez <ogonzal87@gmail.com>
2016-04-08 18:50:27 -0400

    dev enviroment finished - SASS, bootstrap, ui.router, angular, firebase, angularfire, gulp, npm, bower

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 angular.module('YolkoApp', [
-  'ui.router',
-  'firebase'
+  "firebase",
+  "ui.router"
 ]);
 
 angular.module('YolkoApp')
@@ -8,8 +8,20 @@ angular.module('YolkoApp')
   $stateProvider
   	.state('landingPage', {
   		url: '/',
-  		templateUrl: 'views/landing-page/landing-page.html'
+  		templateUrl: 'views/landing-page/landing-page.html',
+      controller: 'SampleCtrl'
   	});
 
   $urlRouterProvider.otherwise('/');
+});
+
+
+
+angular.module('YolkoApp')
+.controller("SampleCtrl", function($scope, $firebaseObject) {
+  var ref = new Firebase("https://yolkoapp.firebaseio.com/oscar");
+
+  var oscar = $firebaseObject(ref);
+
+  oscar.$bindTo($scope, "oscar");
 });
