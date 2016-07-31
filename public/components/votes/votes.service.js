f3c171fe648fe0c48f9ffd8cab0b9e78968b128f
OSCAR GONZALEZ <ogonzal87@gmail.com>
2016-07-30 22:14:09 -0400

    commiting latest changes - need to create branches

---
+++ public/components/votes/votes.service.js
@@ -0,0 +1,27 @@
+angular.module('YolkoApp')
+.service('VotesService', VotesService);
+
+function VotesService(FIREBASE_URL, $firebaseArray, $firebaseObject) {
+	var allVotesApiUrl = new Firebase(FIREBASE_URL + "/votes");
+	var allLikeVotesApiUrl = new Firebase(FIREBASE_URL + "/votes/likes");
+	var allDislikeVotesApiUrl = new Firebase(FIREBASE_URL + "/votes/dislikes");
+
+	// create an Array with all the attendees in the database with theis keys
+	var votes = $firebaseArray(allVotesApiUrl);
+    var likeVotesArray = $firebaseArray(allLikeVotesApiUrl);
+    var dislikeVotesArray = $firebaseArray(allDislikeVotesApiUrl);
+
+    return {
+        allVotesApiUrl: allVotesApiUrl,
+        votes: votes,
+
+        //Likes
+        allLikeVotesApiUrl: allLikeVotesApiUrl,
+        likeVotesArray: likeVotesArray,
+
+        //Dislikes
+        allDislikeVotesApiUrl: allDislikeVotesApiUrl,
+        dislikeVotesArray: dislikeVotesArray,
+
+    };
+}
