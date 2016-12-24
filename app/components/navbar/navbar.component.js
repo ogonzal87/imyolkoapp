fb91001360889e12af248115f94c939e35202efc
OSCAR GONZALEZ <ogonzal87@gmail.com>
2016-12-23 19:49:55 -0500

    nav bar component

---
+++ app/components/navbar/navbar.component.js
@@ -0,0 +1,9 @@
+angular.module('YolkoApp')
+.component('ogNavbar', {
+	templateUrl: "components/navbar/navbar.html",
+	controller: ogNavbarCtrl,
+	bindings: {}
+});
+
+function ogNavbarCtrl ($stateParams) {
+}
